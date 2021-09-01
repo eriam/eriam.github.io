a7b94236342a243f9f5e4874269d3125e2afca52
Eriam Schaffter <eriam@mediavirtuel.com>
2021-09-01 20:39:45 +0200

    OK language

--- blocky/code.js
+++ blocky/code.js
@@ -538,7 +538,6 @@ Code.initLanguage = function() {
 
   // Inject language strings.
   document.title += ' ' + MSG['title'];
-  document.getElementById('title').textContent = MSG['title'];
   document.getElementById('tab_blocks').textContent = MSG['blocks'];
 
   document.getElementById('linkButton').title = MSG['linkTooltip'];
